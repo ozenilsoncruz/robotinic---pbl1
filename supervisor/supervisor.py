eabdc6366e89c2c0069c41efd8b1255266bd741a
Anderson Lima <54255790+andersonlima7@users.noreply.github.com>
2024-10-31 17:32:54 -0300

    feat: supervisor

--- supervisor/supervisor.py
+++ supervisor/supervisor.py
@@ -15,7 +15,7 @@ robot_position = {"x": 50, "y": 50}
 @app.get("/")
 async def get():
     try:
-        with open("index.html", "r", encoding='utf-8') as f:
+        with open("index.html", "r", encoding="utf-8") as f:
             html_content = f.read()
         return HTMLResponse(html_content)
     except FileNotFoundError:
@@ -25,37 +25,38 @@ async def get():
 @app.websocket("/ws/position")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    
-    # brick = conectar_nxt(ENDERECO)
-    # if not brick:
-    #     await websocket.send_json({"error": "Não foi possível conectar ao NXT."})
-    #     await websocket.close()
-    #     return
-    
-    # enviar_msg(brick, "Iniciar")
-    
+
+    brick = conectar_nxt(ENDERECO)
+    if not brick:
+        await websocket.send_json({"error": "Não foi possível conectar ao NXT."})
+        await websocket.close()
+        return
+
+    enviar_msg(brick, "Iniciar")
+
     try:
         while True:
-            robot_position['x'] = 20
-            robot_position['y'] = 0 
-            await websocket.send_json(robot_position)
-            # msg = await receber_msg(brick)
-            # if msg:
-            #     try:
-            #         data = loads(msg)
-            #         robot_position['x'] = int(data.get('x', 0)) / 1000
-            #         robot_position['y'] = int(data.get('y', 0)) / 1000
-            #         await websocket.send_json(robot_position)
-            #     except (ValueError, TypeError) as e:
-            #         print(f"Mensagem: {msg}")
+            # robot_position['x'] = 20
+            # robot_position['y'] = 0
+            # await websocket.send_json(robot_position)
+            msg = await receber_msg(brick)
+            if msg:
+                try:
+                    data = loads(msg)
+                    robot_position["x"] = int(data.get("x", 0)) / 1000
+                    robot_position["y"] = int(data.get("y", 0)) / 1000
+                    await websocket.send_json(robot_position)
+                except (ValueError, TypeError) as e:
+                    print(f"Mensagem: {msg}")
             await asyncio.sleep(0.5)
     except Exception as e:
         print(f"Erro na conexão com NXT. \n{e}")
-        await websocket.close()  
+        await websocket.close()
     finally:
         print("Conexão Bluetooth encerrada.")
 
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="localhost", port=8000)
